6aff3c547c306e302f4a8bc8f89404c5e0cac459
Yogesh Surapaneni <38020646+yogeshsurapaneni@users.noreply.github.com>
2023-04-25 06:20:01 +0000

    touchups

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 sudo apt update
 sudo apt install -y vim tree wget curl ansible
+
+sudo useradd -m ansib -s /bin/bash
+echo "ansib ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo 
+sudo visudo -c
+
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+sudo cp -r ~/.ssh/id_rsa.pub /home/ansib/authorized_keys
